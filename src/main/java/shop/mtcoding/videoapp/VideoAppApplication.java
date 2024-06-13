3b73f7d97967ff78414d7fac4eb85911ad800a3a
yunchocopie <yunnn687@naver.com>
2024-06-13 17:41:02 +0900

    초기 세팅 완료

---
+++ src/main/java/shop/mtcoding/videoapp/VideoAppApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.videoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoAppApplication.class, args);
+    }
+
+}
